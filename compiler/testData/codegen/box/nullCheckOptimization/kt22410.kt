eec9552c178f532a077c70d4612d9d76c47c21b4
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-01-24 14:24:56 +0300

    Reified 'as?' produces nullable result
    Previously, this was treated as a regular CHECKCAST, causing KT-22410.
    
     #Fixed KT-22410 Target versions 1.2.30

---
+++ compiler/testData/codegen/box/nullCheckOptimization/kt22410.kt
@@ -0,0 +1,24 @@
+// FULL_JDK
+
+import java.util.Objects
+
+fun box(): String {
+    defineFunc<String>()
+
+    func(1)
+
+    return if (testedEquals) "OK" else "Fail"
+}
+
+var func: (Any) -> Unit = {}
+
+var testedEquals = false
+
+inline fun <reified T> defineFunc() {
+    func = {
+        val nullable = it as? T
+
+        if (nullable == null)
+            testedEquals = true
+    }
+}
