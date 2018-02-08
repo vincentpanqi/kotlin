c4e0897c6a7b45a890a7d0b46bb91a53845dcc17
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-02-08 12:18:52 +0300

    Skip call to the underlying value of inline class

--- compiler/backend/src/org/jetbrains/kotlin/codegen/StackValue.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/StackValue.java
@@ -295,6 +295,14 @@ public abstract class StackValue {
         return new CollectionElement(collectionElementReceiver, type, kotlinType, getter, setter, codegen);
     }
 
+    public static UnderlyingValueOfInlineClass underlyingValueOfInlineClass(
+            @NotNull Type type,
+            @Nullable KotlinType kotlinType,
+            @NotNull StackValue receiver
+    ) {
+        return new UnderlyingValueOfInlineClass(type, kotlinType, receiver);
+    }
+
     @NotNull
     public static Field field(@NotNull Type type, @NotNull Type owner, @NotNull String name, boolean isStatic, @NotNull StackValue receiver) {
         return field(type, null, owner, name, isStatic, receiver, null);
@@ -1017,6 +1025,24 @@ public abstract class StackValue {
                 @NotNull Type type, @Nullable KotlinType kotlinType, @NotNull InstructionAdapter v
         ) {
             v.aload(this.type);    // assumes array and index are on the stack
+            coerceTo(type, kotlinType, v);
+        }
+    }
+
+    public static class UnderlyingValueOfInlineClass extends StackValueWithSimpleReceiver {
+
+        public UnderlyingValueOfInlineClass(
+                @NotNull Type type,
+                @Nullable KotlinType kotlinType,
+                @NotNull StackValue receiver
+        ) {
+            super(type, kotlinType, false, false, receiver, true);
+        }
+
+        @Override
+        public void putSelector(
+                @NotNull Type type, @Nullable KotlinType kotlinType, @NotNull InstructionAdapter v
+        ) {
             coerceTo(type, kotlinType, v);
         }
     }
