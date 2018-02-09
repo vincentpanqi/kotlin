296f29c5f98060bd4c22ad13c0e95525c32bdf1b
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-02-09 08:26:25 +0300

    Map inline classes in generic argument position to theirs wrap classes

---
+++ compiler/testData/writeSignature/inlineClasses/inlineClassWithComplexSubstitutedType.kt
@@ -0,0 +1,19 @@
+// !LANGUAGE: +InlineClasses
+
+class Inv<T>
+
+inline class AsList<T>(val list: List<T>)
+inline class UInt(val value: Int)
+
+object Test {
+    fun withInlineClassArgument(a: AsList<UInt>) {}
+    fun withListOfInlineClassArgument(a: AsList<List<UInt>>) {}
+}
+
+// method: Test::withInlineClassArgument
+// jvm signature: (Ljava/util/List;)V
+// generic signature: (Ljava/util/List<LUInt;>;)V
+
+// method: Test::withListOfInlineClassArgument
+// jvm signature: (Ljava/util/List;)V
+// generic signature: (Ljava/util/List<+Ljava/util/List<LUInt;>;>;)V
